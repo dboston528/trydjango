54b63afb2e2ce1a14bacc9f84fb683cfeb794f82
DeAndre Boston <dboston3@yahoo.com>
2019-01-17 15:25:14 -0600

    Add new model fields

--- src/products/models.py
+++ src/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title          = models.TextField()
-    description    = models.TextField()
-    cost          = models.TextField()
-    summary        = models.TextField(default='this is cool!')
+    title          = models.CharField(max_length=120) #Max of 120 characters required
+    description    = models.TextField(blank=True, null=True)
+    price          = models.DecimalField(decimal_places=2, max_digits=10000, default=00.00)
+    summary        = models.TextField()
