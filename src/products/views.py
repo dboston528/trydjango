7fa2077c4620e7763df3f32ad828388a69790b01
DeAndre Boston <dboston3@yahoo.com>
2019-01-31 17:57:11 -0600

    Create raw HTML form

--- src/products/views.py
+++ src/products/views.py
@@ -4,15 +4,28 @@ from .forms import ProductForm
 
 from .models import Product
 # Create your views here.
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    context = {
-        'form': form
-    }
 
+def product_create_view(request):
+    # print(request.GET)
+    # print(request.POST)
+    if request.method == "POST":
+        my_new_title = request.POST.get('title')
+        print(my_new_title)
+    # Use next line to save to prouct model database
+    # Product.objects.create(title=my_new_title) 
+    context = {}
     return render(request, "products/product_create.html", context)
+
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#     context = {
+#         'form': form
+#     }
+
+#     return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)  
